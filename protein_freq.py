2ed2eb4eae538d0c745aff2064664f93ce36598a
Jason Stajich <jason.stajich@ucr.edu>
2017-10-30 00:32:22 -0700

    executable

---
+++ protein_freq.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+protein_file = 'Saccharomyces_cerevisiae.peps.fa'
+
+
+# Your solution should print out the aggregate frequency of each amino acid across all the sequences in the file
